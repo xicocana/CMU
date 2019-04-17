506930c8069297c0fed329f2ec64c262fbff74ad
xicocana <kanana.traceur@gmail.com>
2019-04-17 21:23:34 +0100

    inicio migracao para REST - create folder com txt a funcionar

--- P2Photo/app/src/main/java/pt/ulisboa/tecnico/p2photo/CreateFolderActivity.java
+++ P2Photo/app/src/main/java/pt/ulisboa/tecnico/p2photo/CreateFolderActivity.java
@@ -1,15 +1,38 @@
 package pt.ulisboa.tecnico.p2photo;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.Scope;
+import com.google.android.gms.tasks.Task;
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.services.drive.Drive;
+import com.google.api.services.drive.DriveScopes;
+
+import java.util.Collections;
 
 import pt.ulisboa.tecnico.p2photo.GoogleUtils.GoogleCreateFolderActivity;
 
 public class CreateFolderActivity extends AppCompatActivity {
+
+    private DriveServiceHelper mDriveServiceHelper;
+    private static final String TAG = "CreateFolderActivity";
+    private static final int REQUEST_CODE_SIGN_IN = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,10 +42,87 @@ public class CreateFolderActivity extends AppCompatActivity {
         Button create_btn = (Button) findViewById(R.id.button3);
         EditText folder_name = (EditText) findViewById(R.id.editText4);
 
+        requestSignIn();
+
         create_btn.setOnClickListener(v -> {
-            Intent intent = new Intent(CreateFolderActivity.this, GoogleCreateFolderActivity.class);
-            intent.putExtra("foldername", folder_name.getText().toString());
+            Intent intent = new Intent(CreateFolderActivity.this, UserOptionsActivity.class);
+
+            if (mDriveServiceHelper != null) {
+                Log.d(TAG, "Creating a file.");
+                SharedPreferences pref = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE);
+                String name = pref.getString("username", null);
+                Task<GoogleDriveFileHolder> res = mDriveServiceHelper.createFolder(name, folder_name.getText().toString(), null);
+                if(res != null){
+                    String message = "Album " + folder_name.getText().toString() + " created";
+                    Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    String message = "Error creating new album";
+                    Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+                }
+            }
+
             startActivity(intent);
+
         });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent resultData) {
+        switch (requestCode) {
+            case REQUEST_CODE_SIGN_IN:
+                if (resultCode == Activity.RESULT_OK && resultData != null) {
+                    handleSignInResult(resultData);
+                }
+                break;
+        }
+
+        super.onActivityResult(requestCode, resultCode, resultData);
+    }
+
+    /**
+     * Starts a sign-in activity using {@link #REQUEST_CODE_SIGN_IN}.
+     */
+    private void requestSignIn() {
+        Log.d(TAG, "Requesting sign-in");
+
+        GoogleSignInOptions signInOptions =
+                new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                        .requestEmail()
+                        .requestScopes(new Scope(DriveScopes.DRIVE_FILE))
+                        .build();
+        GoogleSignInClient client = GoogleSignIn.getClient(this, signInOptions);
+
+        // The result of the sign-in Intent is handled in onActivityResult.
+        startActivityForResult(client.getSignInIntent(), REQUEST_CODE_SIGN_IN);
+    }
+
+    /**
+     * Handles the {@code result} of a completed sign-in activity initiated from {@link
+     * #requestSignIn()}.
+     */
+    private void handleSignInResult(Intent result) {
+        GoogleSignIn.getSignedInAccountFromIntent(result)
+                .addOnSuccessListener(googleAccount -> {
+                    Log.d(TAG, "Signed in as " + googleAccount.getEmail());
+
+                    // Use the authenticated account to sign in to the Drive service.
+                    GoogleAccountCredential credential =
+                            GoogleAccountCredential.usingOAuth2(
+                                    this, Collections.singleton(DriveScopes.DRIVE_FILE));
+                    credential.setSelectedAccount(googleAccount.getAccount());
+                    Drive googleDriveService =
+                            new Drive.Builder(
+                                    AndroidHttp.newCompatibleTransport(),
+                                    new GsonFactory(),
+                                    credential)
+                                    .setApplicationName("Drive API Migration")
+                                    .build();
+
+                    // The DriveServiceHelper encapsulates all REST API and SAF functionality.
+                    // Its instantiation is required before handling any onClick actions.
+                    mDriveServiceHelper = new DriveServiceHelper(googleDriveService);
+                })
+                .addOnFailureListener(exception -> Log.e(TAG, "Unable to sign in.", exception));
     }
 }
