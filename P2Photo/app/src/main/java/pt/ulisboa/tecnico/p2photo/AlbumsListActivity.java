802bf509a78bce2629d7b942e5db051752c16543
z100eduardo <z100eduardo@hotmail.com>
2019-04-20 16:11:42 +0100

    pequeno fix

--- P2Photo/app/src/main/java/pt/ulisboa/tecnico/p2photo/AlbumsListActivity.java
+++ P2Photo/app/src/main/java/pt/ulisboa/tecnico/p2photo/AlbumsListActivity.java
@@ -36,26 +36,35 @@ public class AlbumsListActivity extends AppCompatActivity {
     private void getUserAlbums(String userName) {
 
         ClientServerComms clientServerComms = new ClientServerComms(this.getApplicationContext());
-        clientServerComms.sendGetAlbums(userName);
-        ArrayList<ArrayList<String>> albumsList = (ArrayList<ArrayList<String>>) clientServerComms.getContent();
+        boolean state = clientServerComms.sendGetAlbums(userName);
 
-        ArrayList<String> albumsFromServer = new ArrayList<String>();
-        for (ArrayList<String> iter: albumsList) {
-            albumsFromServer.add(iter.get(0));
+        if(state) {
+            ArrayList<ArrayList<String>> albumsList = (ArrayList<ArrayList<String>>) clientServerComms.getContent();
+
+            ArrayList<String> albumsFromServer = new ArrayList<String>();
+            for (ArrayList<String> iter: albumsList) {
+                albumsFromServer.add(iter.get(0));
+            }
+            ListView albums_list = (ListView) findViewById(R.id.albums_list);
+            ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, albumsFromServer);
+            albums_list.setAdapter(arrayAdapter);
+
+            albums_list.setOnItemClickListener((parent, view, position, id) -> {
+                Intent intent = new Intent(AlbumsListActivity.this, GoogleImageDownloadActivity.class);
+
+                DataHolder dataHolder = DataHolder.getInstance();
+                dataHolder.setTxtDriveID((String) albumsList.get(position).get(1));
+                dataHolder.setTxtDriveID((String) albumsList.get(position).get(2));
+
+                intent.putExtra("album_name", albumsFromServer.get(position));
+                startActivity(intent);
+            });
         }
-        ListView albums_list = (ListView) findViewById(R.id.albums_list);
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, albumsFromServer);
-        albums_list.setAdapter(arrayAdapter);
 
-        albums_list.setOnItemClickListener((parent, view, position, id) -> {
-            Intent intent = new Intent(AlbumsListActivity.this, GoogleImageDownloadActivity.class);
-
-            DataHolder dataHolder = DataHolder.getInstance();
-            dataHolder.setTxtDriveID((String) albumsList.get(position).get(1));
-            dataHolder.setTxtDriveID((String) albumsList.get(position).get(2));
-
-            intent.putExtra("album_name", albumsFromServer.get(position));
+        else {
+            Intent intent = new Intent(AlbumsListActivity.this, UserOptionsActivity.class);
+            AlbumsListActivity.this.finish();
             startActivity(intent);
-        });
+        }
     }
 }
